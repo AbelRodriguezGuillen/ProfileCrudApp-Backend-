a7e0d734b6bbd1043e61ed04753b7efe181a8e2d
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:24:41 -0500

    Additional unit tests for PostController endpoints
    - Added unit tests for each endpoint 
    - Used Mockito to mock the PostRepository, SearchRepository, and MongoOperations object
    - Tested the getAllPosts endpoint by mocking the PostRepository
    - Tested the search endpoint by mocking the SearchRepository
    - Tested the addPost endpoint by mocking the PostRepository
    - Tested the getPostById endpoint when the post is found
    - Tested the getPostById endpoint when the post is not found
    - Tested the deletePostById endpoint
    - Attempted Prettier fmt

--- src/test/java/com/abel/jobListing/JobListingApplicationTests.java
+++ src/test/java/com/abel/jobListing/JobListingApplicationTests.java
@@ -1,13 +1,113 @@
-package com.abel.jobListing;
+package com.abel.jobListing.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class JobListingApplicationTests {
+import com.abel.jobListing.model.Post;
+import com.abel.jobListing.repository.PostRepository;
+import com.abel.jobListing.repository.SearchRepository;
 
-	@Test
-	void contextLoads() {
+public class PostControllerTests {
+
+	@InjectMocks
+	private PostController postController;
+
+	@Mock
+	private PostRepository postRepository;
+
+	@Mock
+	private SearchRepository searchRepository;
+
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.openMocks(this);
 	}
 
-}
+	@Test
+	public void testGetAllPosts() {
+		Post post1 = new Post("1", "Title 1", "Description 1", "Location 1", "Company 1");
+		Post post2 = new Post("2", "Title 2", "Description 2", "Location 2", "Company 2");
+
+		Mockito.when(postRepository.findAll()).thenReturn(List.of(post1, post2));
+
+		List<Post> posts = postController.getAllPosts();
+
+		assertEquals(2, posts.size());
+		assertEquals("Title 1", posts.get(0).getTitle());
+		assertEquals("Title 2", posts.get(1).getTitle());
+	}
+
+	@Test
+	public void testSearch() {
+		Post post1 = new Post("1", "Java Developer", "Looking for a Java developer", "San Francisco", "Acme Inc.");
+		Post post2 = new Post("2", "React Developer", "Looking for a React developer", "New York", "Beta Corp.");
+
+		Mockito.when(searchRepository.findByText("Java")).thenReturn(List.of(post1));
+		Mockito.when(searchRepository.findByText("React")).thenReturn(List.of(post2));
+
+		List<Post> javaPosts = postController.search("Java");
+		assertEquals(1, javaPosts.size());
+		assertEquals("Java Developer", javaPosts.get(0).getTitle());
+
+		List<Post> reactPosts = postController.search("React");
+		assertEquals(1, reactPosts.size());
+		assertEquals("React Developer", reactPosts.get(0).getTitle());
+	}
+
+	@Test
+	public void testAddPost() {
+		Post post = new Post("1", "Title", "Description", "Location", "Company");
+
+		Mockito.when(postRepository.save(post)).thenReturn(post);
+
+		Post addedPost = postController.addPost(post);
+
+		assertEquals(post, addedPost);
+	}
+
+	@Test
+	public void testGetPostByIdFound() {
+		Post post = new Post("1", "Title", "Description", "Location", "Company");
+
+		Mockito.when(postRepository.findById("1")).thenReturn(java.util.Optional.of(post));
+
+		ResponseEntity<Post> responseEntity = postController.getPostById("1");
+
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertNotNull(responseEntity.getBody());
+		assertEquals(post, responseEntity.getBody());
+	}
+
+	@Test
+	public void testGetPostByIdNotFound() {
+		Mockito.when(postRepository.findById("1")).thenReturn(java.util.Optional.empty());
+
+		ResponseEntity<Post> responseEntity = postController.getPostById("1");
+
+		assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+		assertNull(responseEntity.getBody());
+	}
+
+	@Test
+	public void testDeletePostById() {
+		Mockito.doNothing().when
+		(mongoOperations).remove(Mockito.any(), Mockito.eq("JobPost"));
+		ResponseEntity<String> responseEntity = postController.deletePostById("1");
+
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertEquals("Post deleted successfully", responseEntity.getBody());
+	}
